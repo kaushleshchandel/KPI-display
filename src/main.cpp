0553149d89aa1dfd91d8c1a2d9be166c3fab2e95
Kaushlesh Chandel <120183704+boondockadmin@users.noreply.github.com>
2024-02-11 04:49:37 -0600

    save

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,20 @@
 #include "WiFi.h"
 #include <HTTPClient.h>
 #include <WiFiClientSecure.h>
+#include <PubSubClient.h>
+ 
 
 const char *ssid = "AAA";
 const char *password = "608980608980";
+const char *mqtt_server = "monitor.boondockecho.com";
+const char *mqtt_user = "service";
+const char *mqtt_password = "c5WTn3ivp5vs";
+const int mqtt_port = 1883;
+const char *mqtt_topic = "#"; // Change to your MQTT topic
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
 #if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
 #error "The current version is not supported for the time being, please use a version below Arduino ESP32 3.0"
 #endif
@@ -26,81 +37,105 @@ Arduino_GFX *gfx = new Arduino_ST7789(bus, 5 /* RST */, 0 /* rotation */, true /
 int32_t w, h, n, n1, cx, cy, cx1, cy1, cn, cn1;
 uint8_t tsa, tsb, tsc, ds;
 
-String raised;
-String backers;
-
-void fetchPledgedAmount()
+void reconnectWiFi()
 {
 
-    WiFiClientSecure client;
-    // Uncomment the next line to bypass SSL certificate verification (not recommended for production)
-    client.setInsecure();
+    gfx->setCursor(50, 30);
+    // gfx->clear;
+    gfx->setTextSize(2);
+    gfx->setTextColor(BLUE);
+    // Clear the screen
+    gfx->fillScreen(BLACK); // Replace BLACK with the background color of your choice
+    gfx->print("Connecting WiFi...");
+    
+    WiFi.begin(ssid, password);
 
-    HTTPClient http;
-    String url = "https://www.crowdsupply.com/boondock-technologies/boondock-echo";
-
-    http.begin(client, url);
-    int httpCode = http.GET();
-
-    if (httpCode == 200)
+    while (WiFi.status() != WL_CONNECTED)
     {
-        String payloadraised = http.getString();
-        String payloadbackers = payloadraised;
+        delay(1000);
+        Serial.println("Connecting to WiFi...");
+    }
 
-        int startIndexRaised = payloadraised.indexOf("<p class=\"project-pledged\">");
+    Serial.println("Connected to WiFi");
+}
 
-        if (startIndexRaised != -1)
+void reconnect()
+{
+    
+    gfx->setCursor(50, 30);
+    // gfx->clear;
+    gfx->setTextSize(2);
+    gfx->setTextColor(BLUE);
+    // Clear the screen
+    gfx->fillScreen(BLACK); // Replace BLACK with the background color of your choice
+    gfx->print("Connecting MQTT...");
+    
+
+    while (!client.connected())
+    {
+        Serial.print("Attempting MQTT connection...");
+        if (client.connect("ESP32Client"))
         {
-            int endIndex = payloadraised.indexOf("</p>", startIndexRaised);
-            String pledgedSection = payloadraised.substring(startIndexRaised, endIndex);
-            int spanStart = pledgedSection.indexOf("<span>");
-            int spanEnd = pledgedSection.indexOf("</span>", spanStart);
-            String amountRaised = pledgedSection.substring(spanStart + 6, spanEnd);
-            amountRaised.replace("<sup>", "");
-            amountRaised.replace("</sup>", "");
-            raised = amountRaised;
-
-            //  Serial.println("Amount Raised: " + amountRaised);
+            Serial.println("connected");
+            client.subscribe(mqtt_topic); // Subscribe to the topic
         }
         else
         {
-            raised = "-";
-            //  Serial.println("Raised amount element not found.");
-        }
-
-        //<a href="/boondock-technologies/boondock-echo/backers">
-        int startIndexBackers = payloadbackers.indexOf("<a href=\"/boondock-technologies/boondock-echo/backers\"");
-
-        // Fetch the Backers
-        if (startIndexBackers != -1)
-        {
-            int endIndex = payloadbackers.indexOf("\"fact-label\">", startIndexBackers);
-            String backerSection = payloadbackers.substring(startIndexBackers, endIndex);
-        
-            int spanStart = backerSection.indexOf("fact-number\">");
-            int spanEnd = backerSection.indexOf("</span>", spanStart);
-            String amountbackers = backerSection.substring(spanStart + 6, spanEnd);
-             amountbackers.replace("umber\">", "");
-            //  amountbackers.replace("</sup>", "");
-            backers = "Backers " + amountbackers ;
-
-            
-            Serial.println(backers);
-        }
-        else
-        {
-            backers = "-";
-            Serial.println("Backers amount element not found.");
+            Serial.print("failed, rc=");
+            Serial.print(client.state());
+            Serial.println(" try again in 5 seconds");
+            delay(5000);
         }
     }
-    else
+}
+
+String raised;
+String backers;
+bool forceRefersh = false;
+
+void callback(char *topic, byte *message, unsigned int length)
+{
+    String stopic = topic;
+    String smessage = "";
+
+    for (int i = 0; i < length; i++)
     {
-        backers = "-";
-        Serial.print("Failed to retrieve webpage, status code: ");
-        Serial.println(httpCode);
+        smessage = smessage + (char)message[i];
     }
 
-    http.end();
+    if (stopic.startsWith("crowdsupply/raised"))
+    {
+        Serial.println("Updating Raised amount");
+        raised = smessage;
+        forceRefersh = true;
+    }
+
+    if (stopic.startsWith("crowdsupply/backers"))
+    {
+        Serial.println("Updating Backers ");
+        backers = smessage;
+    }
+
+    Serial.print("Message arrived on topic: " + stopic);
+    Serial.print(topic);
+    Serial.println(smessage);
+}
+
+void refreshDisplay()
+{
+    forceRefersh = false;
+    gfx->setCursor(50, 30);
+    // gfx->clear;
+    gfx->setTextSize(6);
+    gfx->setTextColor(WHITE);
+    // Clear the screen
+    gfx->fillScreen(BLACK); // Replace BLACK with the background color of your choice
+    gfx->print(raised);
+    gfx->setCursor(50, 120);
+    // gfx->clear;
+    gfx->setTextSize(3);
+    gfx->setTextColor(YELLOW);
+    gfx->print(backers);
 }
 
 void setup()
@@ -117,31 +152,11 @@ void setup()
 
     gfx->begin();
     gfx->setRotation(1);
+ 
+    reconnectWiFi();
 
-    w = gfx->width();
-    h = gfx->height();
-    n = min(w, h);
-    n1 = n - 1;
-    cx = w / 2;
-    cy = h / 2;
-    cx1 = cx - 1;
-    cy1 = cy - 1;
-    cn = min(cx1, cy1);
-    cn1 = cn - 1;
-    tsa = ((w <= 176) || (h <= 160)) ? 1 : (((w <= 240) || (h <= 240)) ? 2 : 3); // text size A
-    tsb = ((w <= 272) || (h <= 220)) ? 1 : 2;                                    // text size B
-    tsc = ((w <= 220) || (h <= 220)) ? 1 : 2;                                    // text size C
-    ds = (w <= 160) ? 9 : 12;
-
-    WiFi.begin(ssid, password);
-
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(1000);
-        Serial.println("Connecting to WiFi...");
-    }
-
-    Serial.println("Connected to WiFi");
+    client.setServer(mqtt_server, mqtt_port);
+    client.setCallback(callback);
 }
 
 static inline uint32_t micros_start() __attribute__((always_inline));
@@ -153,24 +168,32 @@ static inline uint32_t micros_start()
     return micros();
 }
 
+
+unsigned long previousMillis = 0; // will store last time the display was updated
+const long interval = 10000;      // interval at which to refresh display (milliseconds)
+
 void loop(void)
 {
-    Serial.println(".");
-    gfx->setCursor(50, 30);
-    // gfx->clear;
-    gfx->setTextSize(6);
-    gfx->setTextColor(WHITE);
-    // Clear the screen
-    gfx->fillScreen(BLACK); // Replace BLACK with the background color of your choice
-    fetchPledgedAmount();
-    gfx->print(raised);
 
-    gfx->setCursor(50, 120);
-    // gfx->clear;
-    gfx->setTextSize(3);
-    gfx->setTextColor(YELLOW);
-    gfx->print(backers);
+    if (!WiFi.isConnected())
+        reconnectWiFi();
 
+    if (!client.connected())
+    {
+        reconnect();
+    }
 
-    delay(300000);
+    client.loop();
+
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= interval)
+    {
+        previousMillis = currentMillis;
+        refreshDisplay();
+    }
+
+    if(forceRefersh )
+     refreshDisplay();
+        
+    
 }
